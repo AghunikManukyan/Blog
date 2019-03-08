06682384471cf94a8bdeadc1ffd741d0627cd659
Aghunik Manukyan <aghmanukyan97@gmail.com>
2019-03-08 18:38:46 +0300

    SQL

---
+++ sql.sql
@@ -0,0 +1,56 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.18-log : Database - blog
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`blog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `blog`;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `category` */
+
+insert  into `category`(`id`,`name`) values (1,'animals'),(2,'house'),(3,'car');
+
+/*Table structure for table `post` */
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `pic_url` varchar(255) DEFAULT NULL,
+  `text` text,
+  `title` varchar(255) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKg6l1ydp1pwkmyj166teiuov1b` (`category_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Data for the table `post` */
+
+insert  into `post`(`id`,`description`,`pic_url`,`text`,`title`,`category_id`,`created_date`) values (5,'Cat','1552050842620_cat.jpg','Cat','Cat',1,'2019-03-08 13:14:03'),(6,'Home','1552050875435_home.jpg','Home','Home',2,'2019-03-08 13:14:35'),(7,'Dog','1552052978881_dog.jpg','Dog ','Dog',1,'2019-03-08 13:49:39'),(8,'BMW','1552053264075_car2.jpg','BMW','BMW M5',3,'2019-03-08 13:54:24'),(9,'BMW','1552053298549_car1.jpg',' BMW ','BMW X6',3,'2019-03-08 13:54:59');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
